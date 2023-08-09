a778fcf8a54fc5d4c768f08ea4aac081106f6d92
johnhoysa <johnhoysa@gmail.com>
2023-08-09 12:33:05 -0500

    Image component created for Style Guide

---
+++ src/lib/components/image/Image.svelte
@@ -0,0 +1,13 @@
+<script>
+	export let imageDesktop;
+	export let imageMobile;
+	export let imageAlt;
+</script>
+
+<picture class="w-full h-full">
+	<source media="(min-width: 768px)" srcset={imageDesktop} />
+	<img class="object-cover w-full h-full" src={imageMobile} alt={imageAlt} />
+</picture>
+
+<style>
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,62 +1,51 @@
 <script>
-  
 </script>
 
 <svelte:head>
-  <title>Sample Style Guide</title>
-  <meta name="description" content="Sample Style Guide" />
+	<title>Sample Style Guide</title>
+	<meta name="description" content="Sample Style Guide" />
 </svelte:head>
 
 <section class="p-2">
-  <h3 class="text-3xl">About</h3>
-  <p>A custom style guide to give developers, authors, and clients a peek at how each component might work on a page</p>
+	<h3 class="text-3xl">About</h3>
+	<p>
+		A custom style guide to give developers, authors, and clients a peek at how each component might
+		work on a page
+	</p>
 </section>
 
 <section class="p-2">
-  <h3 class="text-3xl">Atoms</h3>
-  <p>
-    <a
-      href="/style-guide/headline"
-      class="text-blue-600 visited:text-purple-600">Headline</a
-    >
-  </p>
-  <p>
-    <a href="/style-guide/text" class="text-blue-600 visited:text-purple-600"
-      >Text</a
-    >
-  </p>
+	<h3 class="text-3xl">Atoms</h3>
+	<p>
+		<a href="/style-guide/headline" class="text-blue-600 visited:text-purple-600">Headline</a>
+	</p>
+	<p>
+		<a href="/style-guide/text" class="text-blue-600 visited:text-purple-600">Text</a>
+	</p>
+	<p>
+		<a href="/style-guide/image" class="text-blue-600 visited:text-purple-600">Image</a>
+	</p>
 </section>
 
 <section class="p-2">
-  <h3 class="text-3xl">Components</h3>
-  <br />
-  <p>
-    <a href="/style-guide/card" class="text-blue-600 visited:text-purple-600"
-      >Card</a
-    >
-  </p>
-  <p>
-    <a href="/style-guide/cta" class="text-blue-600 visited:text-purple-600"
-      >CTA</a
-    >
-  </p>
-  <p>
-    <a href="/style-guide/hero" class="text-blue-600 visited:text-purple-600"
-      >Hero</a
-    >
-  </p>
-  <p>
-    <a href="/style-guide/modal" class="text-blue-600 visited:text-purple-600"
-      >Modal</a
-    >
-  </p>
-  <p>
-    <a href="/style-guide/form" class="text-blue-600 visited:text-purple-600"
-      >Form Testing</a
-    >
-  </p>
+	<h3 class="text-3xl">Components</h3>
+	<br />
+	<p>
+		<a href="/style-guide/card" class="text-blue-600 visited:text-purple-600">Card</a>
+	</p>
+	<p>
+		<a href="/style-guide/cta" class="text-blue-600 visited:text-purple-600">CTA</a>
+	</p>
+	<p>
+		<a href="/style-guide/hero" class="text-blue-600 visited:text-purple-600">Hero</a>
+	</p>
+	<p>
+		<a href="/style-guide/modal" class="text-blue-600 visited:text-purple-600">Modal</a>
+	</p>
+	<p>
+		<a href="/style-guide/form" class="text-blue-600 visited:text-purple-600">Form Testing</a>
+	</p>
 </section>
 
 <style>
 </style>
-
