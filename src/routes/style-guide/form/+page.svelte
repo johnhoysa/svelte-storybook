ac6d105d99e88d27fc2db2b79373cd1c12b4c563
johnhoysa <johnhoysa@gmail.com>
2023-08-12 14:27:35 -0500

    Form updates to remove unneeded coded

--- src/routes/style-guide/form/+page.svelte
+++ src/routes/style-guide/form/+page.svelte
@@ -1,11 +1,7 @@
 <script>
 	import Form from '$lib/forms/Form.svelte';
-
-	function handleOnSubmit() {
-		console.log("I'm the handleOnSubmit() in App.svelte");
-	}
 </script>
 
 <div class="w-1/2 px-4 pt-0 pb-4 m-4 border rounded-md shadow-sm border-slate-500 bg-slate-100">
-	<Form on:submit={handleOnSubmit} />
+	<Form />
 </div>
